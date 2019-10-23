5c2f879acb5e8ec7082fb6f78114aa3b4cdbb169
Tatuana4444 <47916561+Tatuana4444@users.noreply.github.com>
2019-10-23 14:16:20 +0300

    lab5

---
+++ Клиент/p1/p1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace Client
+{
+    // Тоже сюда закинул интерфейс, можно выделить в отдельную библиотеку
+    [ServiceContract]
+    public interface IMyService
+    {
+        [OperationContract]
+        int GetText(string Str);
+
+        [OperationContract]
+        string GetStr();
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Uri tcpUri = new Uri("http://localhost:8000/MyService");
+            EndpointAddress address = new EndpointAddress(tcpUri);
+            BasicHttpBinding binding = new BasicHttpBinding();
+            ChannelFactory<IMyService> factory = new ChannelFactory<IMyService>(binding, address);
+            IMyService service = factory.CreateChannel();
+            String Str="", Str2="", StrForInter="";
+            while (StrForInter != "3")
+            {
+                Console.WriteLine("\n>>>Нажмите 1, чтобы ввести текст.\n>>>Нажмите 2, чтобы получить множество всех строки.\n>>>Нажмите 3, чтобы выйти");
+                StrForInter = Console.ReadLine();
+                switch (StrForInter)
+                {
+                    case "1":
+                        Console.WriteLine(">>>Введите '<<', чтобы отправить сообщение");
+                        do
+                        {
+                            Str += Str2;
+                            Str2 = Console.ReadLine() + '\n';
+                        }
+                        while (Str2 != "<<\n");
+                        Console.WriteLine(">>Количество слов в тексте: " + service.GetText(Str));
+                        break;
+                    case "2":
+                        Console.Write(service.GetStr());
+                        break;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Сервер/p0/p0/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Text;
+// Обратите внимание, данную библиотеку нужно будет подключить
+using System.ServiceModel;
+
+namespace Server
+{
+    public class lab5
+    {
+
+        [ServiceContract]
+        public interface IMyService
+        {
+            [OperationContract]
+            int GetText(string Str);
+
+            [OperationContract]
+            string GetStr();
+        }
+        // Реализация методов, которые описаны в интерфейсе
+        public static string Mystr="";
+        public static int n = 0;
+        public class MyService : IMyService
+        {
+            
+            public int GetText(string Str)
+            {
+                int k = 0;
+                for (int i = 0; i < Str.Length; i++)
+                {
+                    if ((Str[i] == ' ') && (Str[i + 1] != ' '))
+                        k++;
+                    if (Str[i] == '\n')
+                    {                        
+                        n++;
+                        k++;
+                    }
+                }
+                Mystr += Str;
+                Console.WriteLine(Str);
+                Console.WriteLine(">>Количество слов в тексте: " + k);
+                Console.WriteLine(">>Количество строк в тексте: " + n);
+                //Console.WriteLine(n + " BBBBBBBBBb ");
+                return k;
+            }
+
+            public string GetStr()
+            {
+               // Console.WriteLine(Mystr + " BBBBBBBBBb ");
+                string Str = ">>Количество строк в тексте: " + n+"\n";
+                int j = 0, k = 1;
+                for (int i = 0; i < Mystr.Length; i++)
+                {
+                    if (Mystr[i] == '\n')
+                    {
+                        Str += Mystr.Substring(j, (i-j+1));
+                        j=i+1;
+                        Str += ">>Количество слов в тексте: " + k + '\n';
+                        k = 1;
+                    }
+                    else                    
+                        if ((Mystr[i] == ' ') && (Mystr[i+1] != ' '))
+                            k++;
+                    
+                }
+                Console.WriteLine(Str);
+                return Str;
+            }
+
+
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                ServiceHost host = new ServiceHost(typeof(MyService), new Uri("http://localhost:8000/MyService"));
+                host.AddServiceEndpoint(typeof(IMyService), new BasicHttpBinding(), "");
+                host.Open();
+                Console.WriteLine("Сервер запущен");
+                Console.ReadLine();
+                host.Close();
+            }
+        }
+    }
+}
